d86ec6ba432aabb265685e7a607833d0cb9e0c0c
Dmitriy-Grakovich <gudvin-gora@mail>
2021-10-30 09:29:19 +0300

    add archer ...

--- archer.sol
+++ archer.sol
@@ -8,20 +8,14 @@ pragma AbiHeader expire;
 import 'warunit.sol';
 
 // This is class that describes you smart contract.
-abstract contract warior is warunit {
-   
+abstract contract archer is warunit {
+
     constructor() public {
-        // Check that contract's public key is set
         require(tvm.pubkey() != 0, 101);
-        // Check that message has signature (msg.pubkey() is not zero) and
-        // message is signed with the owner's private key
         require(msg.pubkey() == tvm.pubkey(), 102);
-        // The current smart contract agrees to buy some gas to finish the
-        // current transaction. This actions required to process external
-        // messages, which bring no value (henceno gas) with themselves.
-        tvm.accept();       
+        tvm.accept();
     }
-    //получить атаку
+     //получить атаку
     function ataclevel(uint value) public override checkOwnerAndAccept{
         atac = atac + value;
     }
